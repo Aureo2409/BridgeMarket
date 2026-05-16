cfb4f7b74faa9025ef742fc669ab41a2dcda0376
unknown <hm9225405@gmail.com>
2026-05-16 17:08:17 +0100

    Remove buggy single-process flag

--- bot.js
+++ bot.js
@@ -125,10 +125,7 @@ Se o utilizador solicitar uma compra de dólares, pedir o link da plataforma, ou
 // 3. Inicializa o cliente do WhatsApp
 const client = new Client({
     authStrategy: new LocalAuth(), // Salva a sessão localmente para não precisares de ler o QR sempre
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
-    },
+    webVersionCache: { type: 'local' }, // 🚨 LOCAL: Evita que o bot congele ao tentar baixar do Github
     puppeteer: {
         headless: true, // Força o modo invisível para poupar recursos
         dumpio: false, // 🚨 DESLIGADO: Remove os erros "falsos" do Chromium (D-Bus, GCM) que poluem o terminal
@@ -140,6 +137,7 @@ const client = new Client({
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
             '--no-zygote',
+            '--single-process', // 🚨 MAGIA AQUI: Impede que o Chromium congele sem dar erro ao tentar criar sub-processos no Linux!
             '--disable-gpu',
             '--disable-software-rasterizer',
             '--disable-site-isolation-trials', // 🚨 Poupa MUITA memória RAM para evitar Congelamentos
